33df8d6f298ec70b7ddd23f4b885ef21cbfe1e05
chxxing <choing26@naver.com>
2022-07-27 09:11:30 +0900

    220727

--- step06/js/step06.js
+++ step06/js/step06.js
@@ -1,11 +1,28 @@
 $(function () {
     $('.gnb>ul>li>a').on('click', function (e) {
-        e.preventDefault();
-        $('.sub_menu'), slideUp();
-        $(this).next().stop().slideToggle();
-    })
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+            $('.sub_menu').slideUp();
+            $(this).next().stop().slideToggle();
+        }
+    });
 
     $('.mbtn').on('click', function () {
-        $('.gnb').toggleClass('on');
+        $('.gnb').toggleClass('on')
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on')
+        $('.sub_menu').removeAttr('style')
+    });
+
+    $('.mclone').on('click', function () {
+        var siteMap = $('.gnb>ul').clone().addClass('container');
+        siteMap.appendTo($('body')).wrap('<div class="sitemap"</div>');
+        $('<i class="xi-close"</i>').appendTo(siteMap.parent())
+    });
+
+    $('.xi-close').on('click', function () {
+        console.log('click')
     })
 })
